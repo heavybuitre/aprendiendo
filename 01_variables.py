25d6b69e9b75285ada82130227428d8fbde7a2ca
heavybuitre <heavybuitre@gmail.com>
2024-02-08 14:30:25 -0600

    Se agrega cliente

--- 01_variables.py
+++ 01_variables.py
@@ -1,6 +1,21 @@
 #Variables
-my_variable = " My String Variable"
+my_variable = "My String Variable"
 print(my_variable)
 cliente = "Dell"
 print(cliente)
+kilometros = 5
+print(kilometros)
+bool_variable = False
+print(bool_variable)
+#Concatenación de variables en print
+print(cliente, kilometros, bool_variable)
+my_int_to_str_variable = str(my_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
 
+#Funciones del sistema
+print(len(my_int_to_str_variable))  #Contar caracteres
+print("Kilometros recorridos:",kilometros)
+#Variables en una sola linea
+name, apellido, alias, edad = "Yeicob","Herrera", 'heavybuitre', 39
+print("Me llamo:", name, apellido, "Mi edad es:",edad,'años Y mi alias es:', alias)
