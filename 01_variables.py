3c921a19501f4f45502e54a85586c6a7cdf2a169
heavybuitre <heavybuitre@gmail.com>
2024-02-08 15:14:27 -0600

    Se agregan datos

--- 01_variables.py
+++ 01_variables.py
@@ -17,5 +17,26 @@ print(type(my_int_to_str_variable))
 print(len(my_int_to_str_variable))  #Contar caracteres
 print("Kilometros recorridos:",kilometros)
 #Variables en una sola linea
-name, apellido, alias, edad = "Yeicob","Herrera", 'heavybuitre', 39
+name, apellido, alias, edad = "Yeicob","Herrera", 'heavybuitre', 35.5
 print("Me llamo:", name, apellido, "Mi edad es:",edad,'años Y mi alias es:', alias)
+print(type(edad))
+#Solicitar datos
+name = input('¿Cual es tu nombre?')
+edad = input('¿Cuantos años tienes?')
+print(name )
+print(edad)
+
+#No tiene type fuerte o constante, cambia entre numeros y texto sin problema
+name = 38
+age= 'Yeicob'
+print(name)
+print(age)
+
+#¿Forzamos el tipo? INT(Entero) STR (texto)
+address: str = 'Mi direccion'
+address = True
+address = 5
+adrress = 1.2
+
+
+ 
