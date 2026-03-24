b8ffd2cd3bcfd49a68c16ca04c4842e57345ccba
heavybuitre <heavybuitre@gmail.com>
2026-03-23 19:42:54 -0600

    Descripción de lo que has hecho

--- 00.py
+++ 00.py
@@ -2,16 +2,18 @@
 print("Hola Python")
 print("Hola Mundo")
 
-"""
-Esto es un 
-comentario en 
-varias lineas 
-"""
+"""Esto es un comentario en varias lineas"""
 
 print('Jacob Herrera')
 print(type('Hola Yeicob'))
 #Consultar el tipo de dato
-print(type(1.5 + 2j)) #tipo de dato complejo
+
+sim1 = 825
+sim2 = 625
+print(sim1>=sim2)
+
+
+
 
 
 
